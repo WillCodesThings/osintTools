6d23e59bccf79fa9ef6a8b893564f3ad12a12c39
WillCodesThings <95990125+WillCodesThings@users.noreply.github.com>
2023-10-23 09:13:08 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+
+def getPhoneNumber():
+    pass
+
+
+def getEarthPro():
+    os.system(
+        'start chrome "https://www.google.com/earth/about/versions/download-thank-you/"'
+    )
+
+
+getEarthPro()
